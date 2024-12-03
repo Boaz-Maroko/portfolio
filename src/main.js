86a5eff3b0249032a961d3d277a2cdf8c4a1a9aa
Boaz-Maroko <boazmaroko123@gmail.com>
2024-12-03 22:54:29 +0300

    Changed mobile layout

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import '../styles/style.css';
 import '../styles/components/header.css';
 import '../styles/components/hero.css';
 import '../styles/components/about.css';
-import '../styles/components/featured.css'
+import '../styles/components/featured.css';
+import '../styles/components/projects.css';
+import '../styles/components/contact.css';
+import '../styles/components/mobile-nav.css';
+
 import '../styles/utils.css';
 
